7286ab88473431fe8d859f18470e10dbc31fa519
HappyZ <zhuyanzi@gmail.com>
2019-11-23 20:10:25 -0800

    add adbd mod to ver 1.6.03.09261

--- ramdisk/sbin/mount_ddat.sh
+++ ramdisk/sbin/mount_ddat.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 
 mkdir /tmp/updater
-mount -t vfat -o noatime,umask=000 /dev/block/platform/soc.2/by-name/ddat /tmp/updater
+mount -t vfat -o noatime,umask=000,iocharset=utf8 /dev/block/platform/soc.2/by-name/ddat /tmp/updater
 if [ $? -ne 0 ]
 then
   mkfs.vfat /dev/block/platform/soc.2/by-name/ddat
-  mount -t vfat -o noatime,umask=000 /dev/block/platform/soc.2/by-name/ddat /tmp/updater
+  mount -t vfat -o noatime,umask=000,iocharset=utf8 /dev/block/platform/soc.2/by-name/ddat /tmp/updater
 fi
